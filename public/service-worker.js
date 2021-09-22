e7eee3ec673a6184bb63d158728a3835f584b9b4
Andy <amerhaut+dev@gmail.com>
2021-09-22 17:28:33 -0400

    Added icons, service worker

---
+++ public/service-worker.js
@@ -0,0 +1,22 @@
+const cacheName = "si-pwa-v1.0.0";
+const assets =[
+    '/'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(cacheName).then(cache => {
+        cache.addAll(assets);
+    }))
+});
+
+self.addEventListener('activate', e => {
+
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
